859231744197e8d396d0bd4a825815eecaefa853
andrefmsouza <andrefmsouza@me.com>
2020-10-12 17:26:11 -0300

    Ajuste firebase

--- js/firebase.js
+++ js/firebase.js
@@ -2,14 +2,14 @@ var emailRE = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDzHHtx0RRLQb6biIQd35IM6jdAm83Vg3o",
-  authDomain: "hackathongetnet.firebaseapp.com",
-  databaseURL: "https://hackathongetnet.firebaseio.com",
-  projectId: "hackathongetnet",
-  storageBucket: "hackathongetnet.appspot.com",
-  messagingSenderId: "287167719449",
-  appId: "1:287167719449:web:c497daa1f95c4215166288",
-  measurementId: "G-6SV2448YGS"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
